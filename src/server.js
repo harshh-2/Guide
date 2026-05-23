8bf564c67645a0d0d1c820d2cd6bfe7b72b94f03
harshhhh-2-2 <hd2hd2hd2006@gmail.com>
2026-05-23 19:14:40 +0530

    cron.js added

--- src/server.js
+++ src/server.js
@@ -3,11 +3,15 @@ import {ENV} from "./config/env.js";
 import {db} from "./config/db.js";
 import {and,eq} from "drizzle-orm";
 import {favorites} from "./db/schema.js";
-
+import {job} from "./config/cron.js";
 const app=express();
 const PORT=ENV.PORT;
 
 app.use(express.json());
+
+if(ENV.NODE_ENV === "production") {
+    job.start();
+}
 
 app.post("/api/favorites",async (req,res)=>{
     try{
